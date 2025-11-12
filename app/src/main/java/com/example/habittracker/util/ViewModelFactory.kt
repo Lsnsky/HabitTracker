923fc96a6b44ea465e6732fc6c5a0b41f9d5547b
lsnsky <lsnsky.de@gmail.com>
2025-11-12 21:35:04 +0100

    added Ðupdated ViewModelFactory

--- app/src/main/java/com/example/habittracker/util/ViewModelFactory.kt
+++ app/src/main/java/com/example/habittracker/util/ViewModelFactory.kt
@@ -4,19 +4,35 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.example.habittracker.data.repository.HabitRepository
 import com.example.habittracker.ui.add_edit_habit.AddEditHabitViewModel
+import com.example.habittracker.ui.habit_list.HabitDetailViewModel
 import com.example.habittracker.ui.habit_list.HabitListViewModel
+import androidx.lifecycle.createSavedStateHandle
+import androidx.lifecycle.viewmodel.CreationExtras
 
 // Эта фабрика будет создавать наши ViewModel
 class ViewModelFactory(private val repository: HabitRepository) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        if (modelClass.isAssignableFrom(HabitListViewModel::class.java)) {
-            @Suppress("UNCHECKED_CAST")
-            return HabitListViewModel(repository) as T
+        return create(modelClass, CreationExtras.Empty)
+    }
+
+    override fun <T : ViewModel> create(modelClass: Class<T>, extras: CreationExtras): T {
+
+        val savedStateHandle = extras.createSavedStateHandle()
+
+        return when {
+            modelClass.isAssignableFrom(HabitListViewModel::class.java) -> {
+                @Suppress("UNCHECKED_CAST")
+                HabitListViewModel(repository) as T
+            }
+            modelClass.isAssignableFrom(AddEditHabitViewModel::class.java) -> {
+                @Suppress("UNCHECKED_CAST")
+                AddEditHabitViewModel(repository) as T
+            }
+            modelClass.isAssignableFrom(HabitDetailViewModel::class.java) -> {
+                @Suppress("UNCHECKED_CAST")
+                HabitDetailViewModel(repository, savedStateHandle) as T
+            }
+            else -> throw IllegalArgumentException("Unknown ViewModel class: ${modelClass.name}")
         }
-        if (modelClass.isAssignableFrom(AddEditHabitViewModel::class.java)) {
-            @Suppress("UNCHECKED_CAST")
-            return AddEditHabitViewModel(repository) as T
-        }
-        throw IllegalArgumentException("Unknown ViewModel class")
     }
 }
